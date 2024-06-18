2cff4539c4c6b28dec19c3adcb51a5af1d06ae65
MariiaO28 <idg.masha@gmail.com>
2024-06-18 13:13:13 +0300

    fixed code and minor changes

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,24 +1,26 @@
 import { PATH_DB } from '../constants/contacts.js';
-import { createFakeContact } from '../utils/createFakeContact.js;'
+import { createFakeContact } from '../utils/createFakeContact.js'
+
 import fs from 'fs/promises';
 
 const generateContacts = async (number) => {
     try {
         const data = await fs.readFile(PATH_DB, 'utf-8');
-        const contacts = JSON.parse(data);
+        const dbData = JSON.parse(data);
+
         let newContacts = [];
         for (let i = 0; i < number; i += 1) {
             newContacts.push(createFakeContact());
         }
-        contacts.contacts = contacts.contacts.concat(newContacts);
-        await fs.writeFile(PATH_DB, JSON.stringify(contacts, 'utf-8'));
+        dbData.push(...newContacts)
+        await fs.writeFile(PATH_DB, JSON.stringify(dbData, null, 2), 'utf-8');
 
         console.log(`Successfully added ${number} new contacts.`);
     } catch (error) {
         if (error.code === 'ENOENT') {
-        console.error(`File via ${PATH_DB} doesn't exist.`);
+            console.error(`File via ${PATH_DB} doesn't exist.`);
         } else {
-        console.error('Error generating contacts:', error);
+            console.error('Error generating contacts:', error);
         }
     }
 }
